b67a2de7431ff9b948b2e54ecbe5deee013e1d25
shiqi <813385610@qq.com>
2023-08-09 21:10:56 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Shiqi/>
+</template>
+
+<script>
+import Shiqi from './components/Shiqi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Shiqi
+  }
+}
+</script>
